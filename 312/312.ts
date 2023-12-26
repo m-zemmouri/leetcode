550d015e34a27657b65ce335489711c6d25ca3cd
ZEMMOURI Mohamed <15168829+m-zemmouri@users.noreply.github.com>
2023-12-26 15:56:12 +0100

    2023-12-26 16:00

--- 312/312.ts
+++ 312/312.ts
@@ -1,45 +1,44 @@
 function maxCoins(nums: number[]): number {
-	let n = nums?.length
-
-	if (!n) return 0
+	if (!nums?.length) return 0
 	// Declar cache
 	let cache = new Map<string, number>()
-	let keys = new Set<string>()
 	// Fill cach
 	nums.forEach((val) => {
-		let key: string = [1, val, 1].sort().join()
-		keys.add(key)
-		cache.set(key, val)
+		cache.set([1, val, 1].sort().join(), val)
 	})
 
-	let newArr: number[] = [1, ...nums, 1]
-	return Coins(newArr)
+	return Coins([1, ...nums, 1])
 
 	function Coins(nums: number[]): number {
 		// // Min nums length ==3
 		let n = nums?.length
-		if (n < 3) return 0
+		// if (n < 3) return 0
 
-		// To optimize
-		if (n === 3) nums = nums.sort()
+		// Check Result if in cache // To optimize
+		let key: string = n === 3 ? nums.sort().join() : nums.join()
 
-		// Check Result if in cache
-		let key: string = nums.join()
-		if (keys.has(key)) return cache.get(key) || 0
+		// if (cache.has(key)) return cache.get(key) || 0
 
 		// Compute Result
 		let val: number = 0
-		// if (n === 3) val = nums[0] * nums[1] * nums[2]
-		// else
+
 		for (let i = 1; i < n - 1; i++) {
-			let left = nums.slice(0, i + 1)
-			let right = nums.slice(i, n)
-			// let v = Coins(left) + nums[i] + Coins(right)
-			let v = Coins(left) + nums[0] * nums[i] * nums[n - 1] + Coins(right)
+			let vLeft = 0
+			if (i > 1) {
+				let left = nums.slice(0, i + 1)
+				let kLeft = left.length === 3 ? left.sort().join() : left.join()
+				vLeft = cache.get(kLeft) || Coins(left)
+			}
+			let vRight = 0
+			if (n - i > 2) {
+				let right = nums.slice(i, n)
+				let kRight = right.length === 3 ? right.sort().join() : right.join()
+				vRight = cache.get(kRight) || Coins(right)
+			}
+			let v = vLeft + nums[0] * nums[i] * nums[n - 1] + vRight
 			val = Math.max(val, v)
 		}
 
-		keys.add(key)
 		cache.set(key, val)
 		return val
 	}
--- dist/312.js
+++ dist/312.js
@@ -1,39 +1,43 @@
 "use strict";
-let cache = new Map();
-let keys = new Set();
 function maxCoins(nums) {
     let n = nums?.length;
     if (!n)
         return 0;
+    // Declar cache
+    let cache = new Map();
+    // let keys = new Set<string>()
     // Fill cach
-    // for (let i = 0; i < n; i++) cache.set(`1, ${nums[i]}, 1`, nums[i])
+    nums.forEach((val) => {
+        let key = [1, val, 1].sort().join();
+        // keys.add(key)
+        cache.set(key, val);
+    });
     let newArr = [1, ...nums, 1];
     return Coins(newArr);
     function Coins(nums) {
         // // Min nums length ==3
         let n = nums?.length;
-        if (n < 3)
-            return 0;
+        // if (n < 3) return 0
         // To optimize
         if (n === 3)
             nums = nums.sort();
         // Check Result if in cache
-        let key = nums.join(',');
-        if (keys.has(key))
-            return cache.get(key) || 0;
+        let key = nums.join();
+        // if (cache.has(key)) return cache.get(key) || 0
         // Compute Result
         let val = 0;
-        if (n === 3)
-            val = nums[0] * nums[1] * nums[2];
-        else
-            for (let i = 1; i < n - 1; i++) {
-                let left = nums.slice(0, i + 1);
-                let right = nums.slice(i, n);
-                // let v = Coins(left) + nums[i] + Coins(right)
-                let v = Coins(left) + nums[0] * nums[i] * nums[n - 1] + Coins(right);
-                val = Math.max(val, v);
-            }
-        keys.add(key);
+        // if (n === 3) val = nums[0] * nums[1] * nums[2]
+        // else
+        for (let i = 1; i < n - 1; i++) {
+            let left = nums.slice(0, i + 1);
+            let right = nums.slice(i, n);
+            // let v = Coins(left) + nums[i] + Coins(right)
+            let vLeft = i < 2 ? 0 : cache.get(key) || Coins(nums.slice(0, i + 1));
+            let vRight = n - i < 3 ? 0 : cache.get(key) || Coins(nums.slice(i, n));
+            let v = vLeft + nums[0] * nums[i] * nums[n - 1] + vRight;
+            val = Math.max(val, v);
+        }
+        // keys.add(key)
         cache.set(key, val);
         return val;
     }
