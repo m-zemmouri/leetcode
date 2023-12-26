984ffbcfe725fe7e07a878eb23ae65e400795185
ZEMMOURI Mohamed <m.zemmouri@gmail.com>
2023-12-26 22:08:02 +0100

    2023-12-26 22:00

--- 312/312.ts
+++ 312/312.ts
@@ -1,32 +1,36 @@
 function maxCoins(nums: number[]): number {
 	if (!nums?.length) return 0
 	// Declar cache
-	let cache = new Map<string, number>()
+	let cache = new Map<number, number>()
 	// Add default value 1
 	nums = [1, ...nums, 1]
 
 	return Coins(0, nums.length)
 
-	function getVal(start: number, end: number): number {
+	function Coins(start: number, end: number): number {
 		if (end - start < 3) return 0
-		const key = `${start}-${end}`
+		const key = start + 301 * end // start < 301
 		// get val from cache or compute it
-		return cache.get(key) || Coins(start, end)
+		let val = cache.get(key)
+		if (val === undefined) {
+			val = Compute(start, end)
+			//Save val in cache
+			cache.set(key, val)
+		}
+		return val
 	}
 
-	function Coins(start: number, end: number): number {
+	function Compute(start: number, end: number): number {
 		let val: number = 0
 
 		for (let i = start + 1; i < end - 1; i++) {
-			const vLeft = getVal(start, i + 1)
-			const vRight = getVal(i, end)
+			const vLeft = Coins(start, i + 1)
+			const vRight = Coins(i, end)
 			const v = vLeft + nums[start] * nums[i] * nums[end - 1] + vRight
 			// get Max val
-			if (v > val) val = v
+			if (val < v) val = v
 		}
-		//Save val in cache
-		const key = `${start}-${end}`
-		cache.set(key, val)
+
 		return val
 	}
 }
--- dist/312.js
+++ dist/312.js
@@ -1,35 +1,33 @@
-"use strict";
-function maxCoins(a) {
-    if (!a?.length)
-        return 0;
-    // Declar cache
-    let cache = new Map();
-    // Fill cach
-    // nums0.forEach((val) => {
-    // 	cache.set([1, val, 1].sort().join(), val)
-    // })
-    a = [1, ...a, 1];
-    return Coins(0, a.length);
-    function getVal(start, end) {
-        if (end - start < 3)
-            return 0;
-        const key = `${start}-${end}`;
-        return cache.get(key) || Coins(start, end);
-    }
-    function Coins(start, end) {
-        let val = 0;
-        for (let i = start + 1; i < end - 1; i++) {
-            const vLeft = getVal(start, i + 1);
-            const vRight = getVal(i, end);
-            const v = a[start] * a[i] * a[end - 1];
-            val = Math.max(val, vLeft + v + vRight);
-        }
-        const key = `${start}-${end}`;
-        cache.set(key, val);
-        return val;
-    }
+'use strict'
+function maxCoins(nums) {
+	if (!nums?.length) return 0
+	// Declar cache
+	let cache = new Map()
+	// Add default value 1
+	nums = [1, ...nums, 1]
+	return Coins(0, nums.length)
+	function Coins(start, end) {
+		if (end - start < 3) return 0
+		const key = start + 301 * end // start < 301
+		// get val from cache or compute it
+		let val = cache.get(key)
+		if (val === undefined) {
+			val = Compute(start, end)
+			//Save val in cache
+			cache.set(key, val)
+		}
+		return val
+	}
+	function Compute(start, end) {
+		let val = 0
+		for (let i = start + 1; i < end - 1; i++) {
+			const vLeft = Coins(start, i + 1)
+			const vRight = Coins(i, end)
+			const v = vLeft + nums[start] * nums[i] * nums[end - 1] + vRight
+			// get Max val
+			if (val < v) val = v
+		}
+		return val
+	}
 }
-const nums5 = [1, 5];
-const v = maxCoins(nums5);
-console.log(v);
 //# sourceMappingURL=312.js.map
