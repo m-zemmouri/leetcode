12b64f2de6b7021471163dfe52948640ca160037
ZEMMOURI Mohamed <15168829+m-zemmouri@users.noreply.github.com>
2023-12-26 18:43:39 +0100

    20231-12-26 19:00

--- 312/312.ts
+++ 312/312.ts
@@ -2,6 +2,7 @@ function maxCoins(nums: number[]): number {
 	if (!nums?.length) return 0
 	// Declar cache
 	let cache = new Map<string, number>()
+	// Add default value 1
 	nums = [1, ...nums, 1]
 
 	return Coins(0, nums.length)
@@ -9,6 +10,7 @@ function maxCoins(nums: number[]): number {
 	function getVal(start: number, end: number): number {
 		if (end - start < 3) return 0
 		const key = `${start}-${end}`
+		// get val from cache or compute it
 		return cache.get(key) || Coins(start, end)
 	}
 
@@ -19,9 +21,10 @@ function maxCoins(nums: number[]): number {
 			const vLeft = getVal(start, i + 1)
 			const vRight = getVal(i, end)
 			const v = vLeft + nums[start] * nums[i] * nums[end - 1] + vRight
+			// get Max val
 			if (v > val) val = v
 		}
-
+		//Save val in cache
 		const key = `${start}-${end}`
 		cache.set(key, val)
 		return val
