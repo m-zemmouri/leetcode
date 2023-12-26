29ea9d57ea5075e5c1b234fa096dab0d4b455ef8
ZEMMOURI Mohamed <15168829+m-zemmouri@users.noreply.github.com>
2023-12-26 11:09:36 +0100

    2023-12-26

--- 312/312.ts
+++ 312/312.ts
@@ -2,41 +2,49 @@ function maxCoins(nums: number[]): number {
 	let n = nums?.length
 
 	if (!n) return 0
+	// Declar cache
 	let cache = new Map<string, number>()
 	let keys = new Set<string>()
 	// Fill cach
-	// for (let i = 0; i < n; i++) cache.set(`1, ${nums[i]}, 1`, nums[i])
+	nums.forEach((val) => {
+		let key: string = [1, val, 1].sort().join()
+		keys.add(key)
+		cache.set(key, val)
+	})
 
 	let newArr: number[] = [1, ...nums, 1]
 	return Coins(newArr)
 
 	function Coins(nums: number[]): number {
 		// // Min nums length ==3
-		// let n = nums?.length
-		// if (n < 3) return 0
+		let n = nums?.length
+		if (n < 3) return 0
+
+		// To optimize
+		if (n === 3) nums = nums.sort()
 
 		// Check Result if in cache
-		let key: string = nums?.join(',')
+		let key: string = nums.join()
 		if (keys.has(key)) return cache.get(key) || 0
+
 		// Compute Result
 		let val: number = 0
-		if (n === 3) val = nums[0] * nums[1] * nums[2]
-		else
-			for (let i = 1; i < n - 1; i++) {
-				val = Math.max(val, nums[i - 1] * nums[i] * nums[i + 1] + Coins(remove(nums, i)))
-			}
+		// if (n === 3) val = nums[0] * nums[1] * nums[2]
+		// else
+		for (let i = 1; i < n - 1; i++) {
+			let left = nums.slice(0, i + 1)
+			let right = nums.slice(i, n)
+			// let v = Coins(left) + nums[i] + Coins(right)
+			let v = Coins(left) + nums[0] * nums[i] * nums[n - 1] + Coins(right)
+			val = Math.max(val, v)
+		}
 
 		keys.add(key)
 		cache.set(key, val)
 		return val
-
-		function remove(nums: number[], i: number): number[] {
-			let a = [...nums]
-			a.splice(i, 1)
-			return a
-		}
 	}
 }
 let nums = [8, 3, 4, 3, 5, 0, 5, 6, 6, 2, 8, 5, 6, 2, 3, 8, 3, 5, 1, 0, 2]
 nums = [3, 1, 5, 8]
-console.log(maxCoins(nums))
+let v = maxCoins(nums)
+console.log(v)
--- dist/312.js
+++ dist/312.js
@@ -1,40 +1,45 @@
 "use strict";
+let cache = new Map();
+let keys = new Set();
 function maxCoins(nums) {
     let n = nums?.length;
     if (!n)
         return 0;
-    let cache = new Map();
+    // Fill cach
+    // for (let i = 0; i < n; i++) cache.set(`1, ${nums[i]}, 1`, nums[i])
     let newArr = [1, ...nums, 1];
     return Coins(newArr);
     function Coins(nums) {
-        // Min nums length ==3
+        // // Min nums length ==3
         let n = nums?.length;
         if (n < 3)
             return 0;
+        // To optimize
+        if (n === 3)
+            nums = nums.sort();
         // Check Result if in cache
-        let key = nums?.join(',');
-        if (cache.has(key)) {
-            console.log(key + ':' + cache.get(key));
+        let key = nums.join(',');
+        if (keys.has(key))
             return cache.get(key) || 0;
-        }
         // Compute Result
         let val = 0;
         if (n === 3)
             val = nums[0] * nums[1] * nums[2];
         else
             for (let i = 1; i < n - 1; i++) {
-                val = Math.max(val, nums[i - 1] * nums[i] * nums[i + 1] + Coins(remove(nums, i)));
+                let left = nums.slice(0, i + 1);
+                let right = nums.slice(i, n);
+                // let v = Coins(left) + nums[i] + Coins(right)
+                let v = Coins(left) + nums[0] * nums[i] * nums[n - 1] + Coins(right);
+                val = Math.max(val, v);
             }
+        keys.add(key);
         cache.set(key, val);
         return val;
-        function remove(nums, i) {
-            let a = [...nums];
-            a.splice(i, 1);
-            return a;
-        }
     }
 }
 let nums = [8, 3, 4, 3, 5, 0, 5, 6, 6, 2, 8, 5, 6, 2, 3, 8, 3, 5, 1, 0, 2];
 nums = [3, 1, 5, 8];
-console.log(maxCoins(nums));
+let v = maxCoins(nums);
+console.log(v);
 //# sourceMappingURL=312.js.map
