1feffbeccb418115f2ec498eb6d282a38ec64472
ZEMMOURI Mohamed <15168829+m-zemmouri@users.noreply.github.com>
2024-01-03 18:59:57 +0100

    2024-01-03 19:00

--- 4/Fast.ts
+++ 4/Fast.ts
@@ -1,32 +1,21 @@
 function findMedianSortedArrays(nums1: number[], nums2: number[]): number {
 	const l1 = nums1.length
 	const l2 = nums2.length
+	const l3 = l1 + l2
+	const merged: number[] = new Array(l3)
 
-	const max1 = nums1[l1 - 1]
-	const max2 = nums2[l2 - 1]
+	let i = 0,
+		j = 0,
+		k = 0
 
-	let merged: number[] = []
-	let i: number = 0,
-		j: number = 0
+	while (i < l1 && j < l2)
+		if (nums1[i] < nums2[j]) merged[k++] = nums1[i++]
+		else merged[k++] = nums2[j++]
 
-	while (i < l1 && j < l2) {
-		if (nums1[i] >= max2) {
-			merged.push(...nums2.slice(j))
-			j = l2
-			break
-		}
-		if (nums2[j] >= max1) {
-			merged.push(...nums1.slice(i))
-			i = l1
-			break
-		}
-		if (nums1[i] < nums2[j]) {
-			merged.push(nums1[i++])
-		} else merged.push(nums2[j++])
-	}
-	let l: number = merged.push(...nums1.slice(i), ...nums2.slice(j))
+	while (i < l1) merged[k++] = nums1[i++]
 
-	let mid: number = Math.floor(l / 2)
-	if (l % 2 === 0) return (merged[mid - 1] + merged[mid]) / 2
-	else return merged[mid]
+	while (j < l2) merged[k++] = nums2[j++]
+
+	const mid = Math.floor(l3 / 2)
+	return l3 % 2 ? merged[mid] : (merged[mid - 1] + merged[mid]) / 2
 }
