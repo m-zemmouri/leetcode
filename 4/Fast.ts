10b89f84b134c08b35bb49168492dfe32b658498
ZEMMOURI Mohamed <15168829+m-zemmouri@users.noreply.github.com>
2024-01-09 14:41:58 +0100

    2024-01-09 15:00

--- 4/Fast.ts
+++ 4/Fast.ts
@@ -3,31 +3,14 @@ function findMedianSortedArrays(nums1: number[], nums2: number[]): number {
 	const l2 = nums2.length
 	const l3 = l1 + l2
 
-	let i: number = l1 ? Math.ceil(l1 / 2) - 1 : 0
-	let j: number = l2 ? Math.ceil(l2 / 2) - 1 : 0
-	let k: number = Math.max(i + j)
+	let i: number = 0
+	let j: number = 0
+	let k: number = 0
 	const mid2 = Math.floor(l3 / 2)
 	let mid2_inNum1: boolean = false
-	if (l3 % 2) {
-		do {
-			k++
-			if (i === l1) {
-				j++
-				mid2_inNum1 = false
-				continue
-			}
-			if (j === l2 || nums1[i] < nums2[j]) {
-				i++
-				mid2_inNum1 = true
-			} else {
-				j++
-				mid2_inNum1 = false
-			}
-		} while (k <= mid2)
-		return mid2_inNum1 ? nums1[i - 1] : nums2[j - 1]
-	}
 	let mid1_inNum1: boolean = false
-	do {
+
+	while (k <= mid2) {
 		mid1_inNum1 = mid2_inNum1
 		k++
 		if (i === l1) {
@@ -42,12 +25,11 @@ function findMedianSortedArrays(nums1: number[], nums2: number[]): number {
 			j++
 			mid2_inNum1 = false
 		}
-	} while (k <= mid2)
+	}
 	i--
 	j--
+	if (l3 % 2) return mid2_inNum1 ? nums1[i] : nums2[j]
 	if (mid1_inNum1 != mid2_inNum1) return (nums1[i] + nums2[j]) / 2
 	if (mid1_inNum1) return (nums1[i - 1] + nums1[i]) / 2
 	return (nums2[j - 1] + nums2[j]) / 2
 }
-
-const result = findMedianSortedArrays([1], [2, 3, 4])
--- dist/4/Fast.js
+++ dist/4/Fast.js
@@ -3,30 +3,29 @@ function findMedianSortedArrays(nums1, nums2) {
     const l1 = nums1.length;
     const l2 = nums2.length;
     const l3 = l1 + l2;
-    let i = l1 ? Math.ceil(l1 / 2) - 1 : 0;
-    let j = l2 ? Math.ceil(l2 / 2) - 1 : 0;
-    let k = Math.min(i + j);
     const mid2 = Math.floor(l3 / 2);
-    let mid2_inNum1 = false;
     if (l3 % 2) {
+        let i = Math.min(Math.floor(l1 / 2), Math.floor(l2 / 2));
+        let j = i;
+        let k = i * 2;
+        let v = 0;
         while (k <= mid2) {
             k++;
             if (i === l1) {
-                j++;
-                mid2_inNum1 = false;
+                v = nums2[j++];
                 continue;
             }
-            if (j === l2 || nums1[i] < nums2[j]) {
-                i++;
-                mid2_inNum1 = true;
-            }
-            else {
-                j++;
-                mid2_inNum1 = false;
-            }
+            if (j === l2 || nums1[i] < nums2[j])
+                v = nums1[i++];
+            else
+                v = nums2[j++];
         }
-        return mid2_inNum1 ? nums1[i - 1] : nums2[j - 1];
+        return v;
     }
+    let i = 0;
+    let j = 0;
+    let k = 0;
+    let mid2_inNum1 = false;
     let mid1_inNum1 = false;
     while (k <= mid2) {
         mid1_inNum1 = mid2_inNum1;
@@ -53,5 +52,6 @@ function findMedianSortedArrays(nums1, nums2) {
         return (nums1[i - 1] + nums1[i]) / 2;
     return (nums2[j - 1] + nums2[j]) / 2;
 }
-const result = findMedianSortedArrays([1], [2, 3, 4]);
+const result = findMedianSortedArrays([1, 2], [3, 4, 5]);
+console.log(result);
 //# sourceMappingURL=Fast.js.map
