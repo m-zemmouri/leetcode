02cfa3b831e2f37df982ac411573f6f227471736
ZEMMOURI Mohamed <15168829+m-zemmouri@users.noreply.github.com>
2024-02-17 09:48:30 +0100

    2024-02-17 10:00

--- 84/Fast2.ts
+++ 84/Fast2.ts
@@ -3,38 +3,27 @@ function largestRectangleArea2(heights: number[]): number {
 
 	let len: number = heights.length
 
-	let result: number = Math.max(...heights)
-	// @ts-ignore
-	let v0: number = heights.shift()
-	let v1: number = 0
-	// let cache: number[] = []
-
-	// heights.map((height, index) => {
-	// 	cache[index] = height
-	// 	result = Math.max(result, height)
-	// })
+	let result: number = 0
+	let v: number = 0
 	let l = 1
-	let i = 0
-	len--
+
 	while (heights.length) {
 		// @ts-ignore
-		v1 = heights.shift()
-		const minHeight = Math.min(v0, v1)
-		result = Math.max(result, l * minHeight)
-		heights.push(minHeight)
-		v0 = v1
-		i++
-		if (i > len) break
-		if (i == len) {
-			len--
-			i = 0
-			l++
+		v = heights.shift()
+		result = Math.max(result, l * v)
+		len--
+		for (let i = 0; i < len; i++) {
+			heights.push(Math.min(v, heights[0]))
+			// @ts-ignore
+			v = heights.shift()
+			result = Math.max(result, l * v)
 		}
+		l++
 	}
 
 	return result
 }
 
-let h2 = [1, 2, 2]
+let h2 = [2, 1, 5, 6, 2, 3]
 let v2 = largestRectangleArea2(h2)
 console.log(v2)
