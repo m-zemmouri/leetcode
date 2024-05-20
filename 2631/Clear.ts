e9a3574d532976882e321ce7f875527de21cc782
ZEMMOURI Mohamed <15168829+m-zemmouri@users.noreply.github.com>
2024-05-20 16:50:31 +0100

    2631

---
+++ 2631/Clear.ts
@@ -0,0 +1,16 @@
+declare global {
+	interface Array<T> {
+		groupBy(fn: (item: T) => string): Record<string, T[]>
+	}
+}
+
+Array.prototype.groupBy = function (fn) {
+	let result: Record<string, number[]> = {}
+	const size = this.length
+	for (let i = 0; i < size; i++) {
+		const key = fn(this[i]) // Apply the function to the current element
+		result[key] ||= [] // Initialize the array if it doesn't exist
+		result[key].push(this[i]) // Push the current element to the corresponding array
+	}
+	return result
+}
---
+++ 2631/Fast.ts
@@ -0,0 +1,13 @@
+declare global {
+	interface Array<T> {
+		groupBy(fn: (item: T) => string): Record<string, T[]>
+	}
+}
+
+Array.prototype.groupBy = function (fn) {
+	return this.reduce((group, item) => {
+		const key = fn(item)
+		;(group[key] = group[key] || []).push(item)
+		return group
+	}, {})
+}
