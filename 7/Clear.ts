8e6afb61fce673fb7c05cf1894710d21c3c452b7
ZEMMOURI Mohamed <m.zemmouri@gmail.com>
2024-10-01 21:52:21 +0100

    Reverses integer

---
+++ 7/Clear.ts
@@ -0,0 +1,9 @@
+function reverse_Integer(x: number): number {
+	const isNegative = x < 0
+	let reversed = Math.abs(x).toString().split('').reverse().join('')
+	let result = parseInt(reversed) * (isNegative ? -1 : 1)
+
+	if (result < -(2 ** 31) || result > 2 ** 31 - 1) return 0
+
+	return result
+}
